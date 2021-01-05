419c41b04a35e3520b0be29679a0c509447d7044
Brychan Toda <btoda@Brychans-MacBook-Pro.local>
2021-01-05 12:37:54 +0100

    Updates

---
+++ precache-manifest.514f3773ce375e28e61c5b89e2c5036c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "489ed8f67a528907af510e956db7cfbd",
+    "url": "/index.html"
+  },
+  {
+    "revision": "e2c47efa564bb8b82dee",
+    "url": "/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "63c5afc484ba6dd0262a",
+    "url": "/static/js/2.d575db1d.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/static/js/2.d575db1d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e2c47efa564bb8b82dee",
+    "url": "/static/js/main.a2d059c1.chunk.js"
+  },
+  {
+    "revision": "404ad0c3be48c6116f9f",
+    "url": "/static/js/runtime-main.3a481efb.js"
+  }
+]);
